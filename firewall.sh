66deb64363e8327ba33593432eba7c7ac5eac02e
Warren Voelkl <warren_voelkl@hotmail.com>
2012-02-06 12:29:45 -0800

    added setup rules to firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -4,11 +4,19 @@ WAN=eth0
 LAN=em1
 LOCALIP=192.168.0.243
 LOCALNETWORK='192.168.0.0/24'  #is this format valid?
-#TODO something to allow tcp services
-#TODO something to allow udp services
+#allowed tcp services on following ports #,#,#,#,...  example 21,22,23
+TCPSERVICES = 21,22,53
+UDPSERVICES = 21
+ICMP = 0
+#ICMP ?
 #TODO something to allow icmp services
 
+
 #####DO NOT MODIFY AFTER THIS POINT#####
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables --table nat --append POSTROUTING -o $LAN -j MASQUERADE
+iptables -A FORWARD -i $WAN -j ACCEPT
+
 
 #clear previous settings
 iptables -F
@@ -24,7 +32,7 @@ iptables -N icmp
 #Drop All packets to local computer
 iptables -A INPUT -d LOCALIP -j DROP #verified
 #Drop all sin fin packets
-iptables -A INPUT -p tcp --tcp-flags SYN,FIN -j DROP
+iptables -A FORWARD -p tcp --tcp-flags SYN,FIN -j DROP #untested
 
 #jump all traffic to the appropriate chain
 iptables -p tcp -j tcp
@@ -40,6 +48,8 @@ iptables -p icmp -j icmp
 
 ptables -A tcp -i $WAN -p tcp -m multiport --ports 32768:32775,111:515 -j DROP
 iptables -A udp -i $WAN -p udp -m multiport --ports 32768:32775,137:139 -j DROP
+#allow tcp and udp services
+
 #block all telnet traffic
 iptables -p tcp --sport telnet -j DROP
 iptables -p tcp --dport telnet -j DROP
