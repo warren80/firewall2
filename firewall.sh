54c176dbeb8bdc1eacdd67e0338aa2b9a90691fe
Warren Voelkl <warren_voelkl@hotmail.com>
2012-02-06 14:17:31 -0800

    merging

--- firewall.sh
+++ firewall.sh
@@ -1,19 +1,39 @@
 #set WAN and LAN variables according to ifconfig on dual nic computer
 
+#External Network Interface card
 WAN=eth0
+
+#Internal network Interface card
 LAN=em1
-LOCALIP=192.168.0.243
+
+#address from internal Network
+INTERNALIP=192.168.180.1
+
+#address from external Networks
+EXTERNALIP=192.168.0.11
+
+#address to client computer
+CLIENTIP=192.168.180.12
+
+#External Network subnet mask
 LOCALNETWORK='192.168.0.0/24'  #is this format valid?
-#allowed tcp services on following ports #,#,#,#,...  example 21,22,23
-TCPSERVICES = 21,22,53
-UDPSERVICES = 21
-ICMP = 0
-#ICMP ?
+
+#Allowed tcp services to client computer following ports #,#,#,#,...  example 21,22,23
+TCPSERVICES=21,22,53
+
+#Allowed udp ports to enable services to client computer example 21,22,23
+UDPSERVICES=21,22
+q
+#Allow ICMP protocols 0 Echo reply 8 Echo request etc
+ICMP = 0 8
 #TODO something to allow icmp services
 
 
 #####DO NOT MODIFY AFTER THIS POINT#####
 echo 1 > /proc/sys/net/ipv4/ip_forward
+ifconfig $WAN down
+ifconfig $LAN down
+
 iptables --table nat --append POSTROUTING -o $LAN -j MASQUERADE
 iptables -A FORWARD -i $WAN -j ACCEPT
 
@@ -42,16 +62,24 @@ iptables -p icmp -j icmp
 #TODO block wrong way syns
 #TODO accept fragments
 #TODO accept all packets that belong to an existing connection on allowed ports
-#TODO set minimum delay for FTP and SSH
-#TODO set Maximim Throughput for ftp
+#TODO set minimum delay for FTP and SSH Matt
+#TODO set Maximim Throughput for ftp Matt
 #TODO only allow new and established traffic to go through firewall 'stateful'
+iptables -A F
 
 ptables -A tcp -i $WAN -p tcp -m multiport --ports 32768:32775,111:515 -j DROP
 iptables -A udp -i $WAN -p udp -m multiport --ports 32768:32775,137:139 -j DROP
 #allow tcp and udp services
+iptables -A INPUT -p tcp -m multiport --ports TCPSERVICES -j FORWARD #needs to forward to client
+iptables -A 
 
 #block all telnet traffic
 iptables -p tcp --sport telnet -j DROP
 iptables -p tcp --dport telnet -j DROP
+#ICMP Rules
+iptables -A INPUT -p icmp --icmp-type 8 -s 0/0 -d $EXTERNALIP /
+	-m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -p icmp --icmp-type 0 -s $EXTERNALIP -d 0/0 /
+	-m state --state ESTABLISHED,RELATED -j ACCEPT
 
 
